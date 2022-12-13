02b333c34b391ca0f5b906d0213b89960ba3bded
allecosta <alle_costa10@outlook.com>
2022-12-13 09:55:23 -0300

    add favicon in admin page

--- admin/header.php
+++ admin/header.php
@@ -1,9 +1,10 @@
-<meta content="" name="descriptison">
-  <meta content="" name="keywords">
+
+  <!-- Favicon -->
+  <link href="../assets/img/home.jpg" rel="icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/font-awesome/css/all.min.css">
+  <link rel="stylesheet" href="../assets/font-awesome/css/all.min.css">
 
 
   <!-- Vendor CSS Files -->
@@ -32,7 +33,7 @@
   <script src="../assets/vendor/counterup/counterup.min.js"></script>
   <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
   <script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
-    <script type="text/javascript" src="../assets/font-awesome/js/all.min.js"></script>
+  <script type="text/javascript" src="../assets/font-awesome/js/all.min.js"></script>
   <script type="text/javascript" src="../assets/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
 
 
