35d62f4f2b21ed99e177c11aa027ec032ab2b4c0
allecosta <alle_costa10@outlook.com>
2022-12-13 15:06:33 -0300

    feat: add title

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  	<meta charset="utf-8">
+  	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>Tech Me</title>
 	<?php 
 	
 	include 'header.php';
